3d8307bdb0605873984fa9011f4bd4fcd224b982
Iñaki de Miguel Díaz <imigueldiaz@gmail.com>
2021-04-12 08:21:15 +0200

    Algunos avances menores y añadido .devcontainer sin poder probarlo aún.

--- tools/addmorph.js
+++ tools/addmorph.js
@@ -1,17 +1,40 @@
 const descargar = require("./descargar");
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
-
+const rmfr = require("rmfr");
+const Zip = require("adm-zip");
 const REPOSITORY = "https://github.com/sbosio/rla-es";
 const TAG = "v2.6";
-const LOCALE = "es";
+const LOCALE = "es-ES";
 
 const URLOXT = `${REPOSITORY}/releases/download/${TAG}/${LOCALE}.oxt`;
 const URLSRC = `${REPOSITORY}/archive/refs/tags/${TAG}.zip`;
 
-fs.mkdir("./work", { recursive: true }, (err) => {
-    if (err) throw err;
-});
+const descargarFuentes = async() => {
+    await rmfr("./work");
 
-descargar(URLOXT, path.join("./work", `${LOCALE}.oxt`));
-descargar(URLSRC, path.join("./work", `${TAG}.zip`));
+    await fs.mkdir("./work", { recursive: true }, (err) => {
+        if (err) throw err;
+    });
+
+    await descargar(URLOXT, path.join("./work", `${LOCALE}.oxt`));
+    await descargar(URLSRC, path.join("./work", `${TAG}.zip`));
+
+    let zip = new Zip(path.join("./work", `${LOCALE}.oxt`));
+    zip.extractEntryTo(`${LOCALE}.aff`, "./work", true, true);
+    zip.extractEntryTo(`${LOCALE}.dic`, "./work", true, true);
+
+    zip = new Zip(path.join("./work", `${TAG}.zip`));
+    zip.extractEntryTo("rla-es-2.6/ortografia/palabras/", "./work", true, true);
+    zip.extractEntryTo("rla-es-2.6/ortografia/afijos/", "./work", true, true);
+
+    try {
+        fs.unlink(path.join("./work", `${LOCALE}.oxt`));
+        fs.unlink(path.join("./work", `${TAG}.zip`));
+    } catch (e) {
+        console.error(e);
+    }
+};
+console.log("Descargando las fuentes y descomprimiendo ficheros necesarios...");
+
+descargarFuentes();
