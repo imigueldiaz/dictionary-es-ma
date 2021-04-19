d1b00165e71ca18182579c18f131f769e5d635aa
Iñaki de Miguel Díaz <imigueldiaz@gmail.com>
2021-04-19 19:59:07 +0200

    Se deshace por ahora el trabajo con devcontainers

--- tools/addmorph.js
+++ tools/addmorph.js
@@ -10,30 +10,30 @@ const LOCALE = "es_ES";
 const URLOXT = `${REPOSITORY}/releases/download/${TAG}/${LOCALE}.oxt`;
 const URLSRC = `${REPOSITORY}/archive/refs/tags/${TAG}.zip`;
 
-const descargarFuentes = async () => {
-  await rmfr("./work");
+const descargarFuentes = async() => {
+    await rmfr("./work");
 
-  await fs.mkdir("./work", { recursive: true }, (err) => {
-    if (err) throw err;
-  });
+    await fs.mkdir("./work", { recursive: true }, (err) => {
+        if (err) throw err;
+    });
 
-  await descargar(URLOXT, path.join("./work", `${LOCALE}.oxt`));
-  await descargar(URLSRC, path.join("./work", `${TAG}.zip`));
+    await descargar(URLOXT, path.join("./work", `${LOCALE}.oxt`));
+    await descargar(URLSRC, path.join("./work", `${TAG}.zip`));
 
-  let zip = new Zip(path.join("./work", `${LOCALE}.oxt`));
-  zip.extractEntryTo(`${LOCALE}.aff`, "./work", true, true);
-  zip.extractEntryTo(`${LOCALE}.dic`, "./work", true, true);
+    let zip = new Zip(path.join("./work", `${LOCALE}.oxt`));
+    zip.extractEntryTo(`${LOCALE}.aff`, "./work", true, true);
+    zip.extractEntryTo(`${LOCALE}.dic`, "./work", true, true);
 
-  zip = new Zip(path.join("./work", `${TAG}.zip`));
-  zip.extractEntryTo("rla-es-2.6/ortografia/palabras/", "./work", true, true);
-  zip.extractEntryTo("rla-es-2.6/ortografia/afijos/", "./work", true, true);
+    zip = new Zip(path.join("./work", `${TAG}.zip`));
+    zip.extractEntryTo("rla-es-2.6/ortografia/palabras/", "./work", true, true);
+    zip.extractEntryTo("rla-es-2.6/ortografia/afijos/", "./work", true, true);
 
-  try {
-    fs.unlink(path.join("./work", `${LOCALE}.oxt`));
-    fs.unlink(path.join("./work", `${TAG}.zip`));
-  } catch (e) {
-    console.error(e);
-  }
+    try {
+        fs.unlink(path.join("./work", `${LOCALE}.oxt`));
+        fs.unlink(path.join("./work", `${TAG}.zip`));
+    } catch (e) {
+        console.error(e);
+    }
 };
 console.log("Descargando las fuentes y descomprimiendo ficheros necesarios...");
 
